984c23555bb5c1d9f98f97b66410ed18be1bac6c
kvaikunthan <kvai7277@gmail.com>
2025-04-26 13:24:23 -0700

    reworked exercise class design for modularity

--- backend/pose_estimation/pushupEvaluator.py
+++ backend/pose_estimation/pushupEvaluator.py
@@ -9,7 +9,7 @@ cap = cv2.VideoCapture(0)
 model = Perciever()
 pu = Pushups(model)
 
-window = deque(maxlen=30)
+window = deque(maxlen=20)
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -21,17 +21,19 @@ while cap.isOpened():
     disp = frame.copy()
 
     currentAngle, pts = pu.grade(frame)
+    pu.countReps(frame)
 
     window.append(currentAngle)
 
     smoothAngle = np.mean(window)
 
-    if smoothAngle < 131:
+    if smoothAngle < 129:
         disp = cv2.putText(disp, f"BAD FORM", (50, 175), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
     else:
         disp = cv2.putText(disp, f"GOOD FORM", (50, 175), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
     disp = cv2.putText(disp, f"Angle: {smoothAngle:.2f}", (50, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    disp = cv2.putText(disp, f"Pushups: {pu.pushupCount}", (300, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
     disp = draw(disp, pts)
     
     cv2.imshow('hi', disp)

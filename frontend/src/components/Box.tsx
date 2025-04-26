c39426be21ad4b3c66d955fa1283927570ee406d
Clinton Nguyen <clintonnguyen@gmail.com>
2025-04-26 00:18:37 -0700

    boxes

---
+++ frontend/src/components/Box.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Box.css'
+
+const Box = () => {
+    return (
+    <div className="container">
+        <div className="box"/>
+
+        <div className="box"/>
+        <div className="box"/>
+    </div>
+    )
+    
+}
+export default Box;
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,6 +1,7 @@
 import LoginButton from "../components/LoginButton";
 import LogoutButton from "../components/LogoutButton";
 import { NavBar } from "../components/Navbar";
+import Box from "../components/Box"
 import "./home.css";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <NavBar />
       <LoginButton />
       <LogoutButton />
+      <Box />
     </div>
   );
 }

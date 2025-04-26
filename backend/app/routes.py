0d58920884dd3e12599f8cb3f07485af7178fcf6
phongtnguyen2006 <124423542+phongtnguyen2006@users.noreply.github.com>
2025-04-26 16:50:57 -0700

    add counter/grade

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, jsonify, request, Response, current_app
+from flask import Blueprint, jsonify, request, Response, current_app, stream_with_context
 from camera import Camera
 import cv2
-
+import time
+import json
 main = Blueprint('main', __name__)
+
+cam = Camera()
 
 @main.route('/api/ping', methods=['GET'])
 def ping():
     return jsonify({'message': 'pong from Flask!'})
 
 def gen_frames():
-    cam = Camera()
     for frame in cam:
         success, jpeg = cv2.imencode('.jpg', frame)
         if not success:
@@ -34,3 +36,12 @@ def mongowrite():
     
     return jsonify({'inserted_id': str(result.inserted_id)}), 201
 
+@main.route('/api/pushup_data_stream')
+def pushup_data_stream():
+    def event_stream():
+        while True:
+            data = json.dumps(cam.latest_data)  # Proper JSON!
+            yield f"data: {data}\n\n"
+            time.sleep(1)
+
+    return Response(stream_with_context(event_stream()), mimetype="text/event-stream")

827e489558072e764b1112f0bf8c478649f79fae
Jordan Junaidi <jordanrjunaidi@gmail.com>
2025-04-25 21:02:36 -0700

    Setup auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <BrowserRouter>
+    <Auth0Provider
+      domain="dev-ggn0ietjt3kd77u5.us.auth0.com"
+      clientId="5c3ZCQrruAcSIhbHxUs5EUxSAn5FEMsF"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>
+);

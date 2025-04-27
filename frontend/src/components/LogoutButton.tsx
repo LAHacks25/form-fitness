b1b5a577b7ed164e896a9ecb1240114c186f3e80
Jordan Junaidi <jordanrjunaidi@gmail.com>
2025-04-27 03:17:46 -0700

    Added redirection after logout

--- frontend/src/components/LogoutButton.tsx
+++ frontend/src/components/LogoutButton.tsx
@@ -1,12 +1,17 @@
 import "./LogoutButton.css";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
+  const navigate = useNavigate();
+
   const handleClick = async () => {
     try {
       const response = await fetch("/api/auth/logout", {
         credentials: "include",
       });
       const data = response.json();
+
+      navigate("/");
     } catch (error) {
       console.error(error);
     }
